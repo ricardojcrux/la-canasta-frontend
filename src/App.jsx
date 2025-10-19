7a53da0b7af255837949c5c6fee9a2bbf66e8abc
Ricardo Zapata Cruz <ricardozapatacruz@gmail.com>
2025-10-19 11:36:31 -0500

    Update deploy pipeline

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import NotFound from "./pages/NotFound";
 import AppRouter from "./routes";
 
 const queryClient = new QueryClient();
